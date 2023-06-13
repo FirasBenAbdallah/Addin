dd64d6d9054b6460eaf3d45fb74d8fa333b1da33
Firasbenabdallah <firas.benabdallah@esprit.tn>
2023-06-13 16:27:27 +0100

    Event && Polls preview

--- app/src/main/java/addinn/dev/team/presentation/events/EventsView.kt
+++ app/src/main/java/addinn/dev/team/presentation/events/EventsView.kt
@@ -1,5 +1,6 @@
 package addinn.dev.team.presentation.events
 
+import addinn.dev.team.presentation.recoverPass.ResetView
 import addinn.dev.team.utils.navigation.NavigationProvider
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,10 +9,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun EventsView(navigator: NavigationProvider) {
+fun EventsView(navigator: NavigationProvider?) {
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
         Text("Events")
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun EventsPreview() {
+    EventsView(null)
 }
--- app/src/main/java/addinn/dev/team/presentation/polls/PollsView.kt
+++ app/src/main/java/addinn/dev/team/presentation/polls/PollsView.kt
@@ -1,5 +1,6 @@
 package addinn.dev.team.presentation.polls
 
+import addinn.dev.team.presentation.events.EventsView
 import addinn.dev.team.utils.navigation.NavigationProvider
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,10 +9,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun PollsView(navigator: NavigationProvider) {
+fun PollsView(navigator: NavigationProvider?) {
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
         Text("Polls")
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ResetPreview() {
+    PollsView(null)
 }
