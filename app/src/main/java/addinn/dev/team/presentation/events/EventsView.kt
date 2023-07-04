f151e48620014527379c2340ccc9d12ef0ee5eb3
FirasBenAbdallah <firas.benabdallah@esprit.tn>
2023-07-04 17:08:27 +0100

    DatePicker

--- app/src/main/java/addinn/dev/team/presentation/events/EventsView.kt
+++ app/src/main/java/addinn/dev/team/presentation/events/EventsView.kt
@@ -1,24 +1,239 @@
 package addinn.dev.team.presentation.events
 
+
+/*
+
 import addinn.dev.team.utils.navigation.NavigationProvider
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.widget.DatePicker
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.*
 
 @Composable
-fun EventsView(navigator: NavigationProvider?) {
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
-        Text("Events")
+fun EventsView(navigator: NavigationProvider) {
+    MainContent()
+}
+
+// Creating a composable
+// function to display Top Bar
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainContent() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("GFG | Date Picker", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(Color(0xff0f9d58))
+            )
+        },
+        content = { MyContent() }
+    )
+}
+
+
+@Suppress("NAME_SHADOWING")
+@Composable
+fun MyContent() {
+
+    // Fetching the Local Context
+    val mContext = LocalContext.current
+
+    // Declaring integer values
+    // for year, month and day
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int
+
+    // Initializing a Calendar
+    val mCalendar = Calendar.getInstance()
+
+    // Fetching current year, month and day
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+    mCalendar.time = Date()
+
+    // Declaring a string value to
+    // store date in string format
+    val mDate = remember { mutableStateOf("") }
+
+    // Declaring DatePickerDialog and setting
+    // initial values as current values (present year, month and day)
+    val mDatePickerDialog = DatePickerDialog(
+        mContext,
+        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth/${mMonth + 1}/$mYear"
+        }, mYear, mMonth, mDay
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // Creating a button that on
+        // click displays/shows the DatePickerDialog
+        Button(onClick = {
+            mDatePickerDialog.show()
+        }, colors = ButtonDefaults.buttonColors(containerColor = Color(0XFF0F9D58))) {
+            Text(text = "Open Date Picker", color = Color.White)
+        }
+
+        // Adding a space of 100dp height
+        Spacer(modifier = Modifier.size(100.dp))
+
+        // Displaying the mDate value in the Text
+        Text(text = "Selected Date: ${mDate.value}", fontSize = 30.sp, textAlign = TextAlign.Center)
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
-fun EventsPreview() {
-    EventsView(null)
+fun DefaultPreview() {
+    MainContent()
+}
+*/
+
+
+
+import addinn.dev.team.utils.navigation.NavigationProvider
+import android.app.DatePickerDialog
+import android.content.Context
+import android.icu.util.Calendar
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun EventsView(navigator : NavigationProvider) {
+    var selectedDate by remember { mutableStateOf<LocalDate?>(null) }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Selected Date: ${selectedDate?.toString() ?: "None"}",
+            style = MaterialTheme.typography.bodySmall
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DatePicker(onDateSelected = { date -> selectedDate = date })
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun DatePicker(onDateSelected: (LocalDate) -> Unit) {
+    val selectedDate = remember { mutableStateOf(LocalDate.now()) }
+
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Selected Date: ${selectedDate.value}",
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
+        Button(
+            onClick = {
+                showDatePickerDialog(context, selectedDate.value, onDateSelected)
+            }
+        ) {
+            Text(text = "Select Date")
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun showDatePickerDialog(
+    context: Context,
+    initialDate: LocalDate,
+    onDateSelected: (LocalDate) -> Unit
+) {
+    val calendar = Calendar.getInstance()
+
+
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+            val selectedDate = LocalDate.of(selectedYear, selectedMonth + 1, selectedDayOfMonth)
+            onDateSelected(selectedDate)
+        },
+        year,
+        month,
+        day
+    )
+
+    datePickerDialog.show()
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun DatePickerExample() {
+    var selectedDate by remember { mutableStateOf<LocalDate?>(null) }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Selected Date: ${
+                selectedDate?.format(
+                    DateTimeFormatter.ofLocalizedDate(
+                        FormatStyle.MEDIUM
+                    )
+                ) ?: "None"
+            }",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DatePicker(onDateSelected = { date -> selectedDate = date })
+    }
 }
